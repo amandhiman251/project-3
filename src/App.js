4361ee7335cb690da6f0c31f1d3835921958ae99
Aman Dhiman <amandhiman251@gmail.com>
2020-11-25 22:47:43 +0530

    Added Routes to load Users and Courses on different pages without links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Users from './container/users/users';
+import Courses from './container/courses/cousres';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Route path='/users' component={Users} />
+        <Route path='/courses' component={Courses} />
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/container/courses/cousres.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const courses = () => <h1>courses</h1>;
+
+export default courses;
