72adbd1c9ce98360d6a1c52e7cb03748f381ceac
Aman Dhiman <amandhiman251@gmail.com>
2020-11-25 23:10:01 +0530

    Added navigation and active css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, NavLink} from 'react-router-dom';
 import Users from './container/users/users';
 import Courses from './container/courses/cousres';
 
@@ -9,6 +9,13 @@ class App extends Component {
     return (
       <BrowserRouter>
       <div className="App">
+        <nav>
+          <ul>
+            <li><NavLink activeClassName='navlink' to='/users'>Users</NavLink></li>
+            <li><NavLink activeStyle={{color:'blue'}} to='courses'>Courses</NavLink></li>
+          </ul>
+
+        </nav>
         <Route path='/users' component={Users} />
         <Route path='/courses' component={Courses} />
       </div>
