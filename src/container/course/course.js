ba9fa0b391fd1561a5bf7999a298e969db2b26b4
Aman Dhiman <amandhiman251@gmail.com>
2020-11-28 00:40:20 +0530

    task 6: load the 'Course' component as a nested component of 'Courses'

--- src/container/course/course.js
+++ src/container/course/course.js
@@ -5,21 +5,36 @@ import axios from 'axios';
 class Course extends Component {
     state={
         course:{},
-        title:''
+        title:'',
+        axios: null
     }
     componentDidMount() {
+        this.clickchangetitleHandler();
+    }
+        
+    componentDidUpdate(){
+        this.clickchangetitleHandler();
+    }
+    clickchangetitleHandler(){
+        let courseid = this.props.match.params.courseid;
+        const query = new URLSearchParams(this.props.location.search);
+        for(let params of query.entries()){
+            if(this.state.title !== params[1]){
+            this.setState({title:params[1]})
+            }
+        } 
+        
+            if (this.state.axios!==courseid){
+                this.setState({axios:courseid});
         axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.match.params.courseid)
         .then(response => {
             this.setState({course:response.data});
-        })
-        const query = new URLSearchParams(this.props.location.search);
-        for(let params of query.entries()){
-            this.setState({title:params[1]})
-        }
-        
+                console.log(response.data);
+        }) 
+        }  
+    }
 
         
-    }
     render(){
     return <div>
             <h1>{this.state.title}</h1>
